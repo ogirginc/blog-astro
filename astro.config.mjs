d99de5ce850632842ac70b561a505224770eeff6
Oğulcan Girginç <ogulcan@girginc.com>
2025-03-22 16:11:46 +0300

    Update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://churchTao.github.io",
   // 部署到 github pages 时，需要设置 base, 自己使用时，可以酌情设置
-  base: "/PlainPage",
+  base: "/",
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
