81cc11b1147d7360f83b90420d6f0ae3701e00fc
Oğulcan Girginç <ogulcan@girginc.com>
2025-03-22 17:02:23 +0300

    Update page info

--- src/consts.ts
+++ src/consts.ts
@@ -2,17 +2,19 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Website Configuration
-export const SITE_TITLE = "PlainPage";
+export const SITE_TITLE = "home of ogirginc";
 export const SITE_DESCRIPTION = "Welcome (. ❛ ᴗ ❛.)";
-export const COPYRIGHT = "© 2024 PlainPage All Rights Reserved";
+export const COPYRIGHT = `© ${new Date().getFullYear()} All Rights Reserved`;
 export const ICP_NUMBER = "";
 
 // Social Media Links, leave empty or comment out = not displayed
 export const SOCIAL_LINKS = {
-  Github: "https://github.com/churchTao",
-  Twitter: "https://twitter.com/yourusername",
-  //   LinkedIn: "https://www.linkedin.com/in/yourusername",
-  //   Instagram: "https://www.instagram.com/yourusername",
+  Github: "https://github.com/ogirginc",
+  Twitter: "https://twitter.com/ogirginc",
+  LinkedIn: "https://www.linkedin.com/in/ogirginc",
+  Bsky: "https://ogirginc.bsky.social",
+  Mastodon: "https://ruby.social/@ogirginc",
+  Instagram: "https://www.instagram.com/ogirginc",
   //   Facebook: "https://www.facebook.com/yourusername",
   //   YouTube: "https://www.youtube.com/yourusername",
 };
@@ -34,9 +36,9 @@ export const NAV_ITEMS = [
 // Blog Configuration
 export const BLOG_CONFIG = {
   locale: "en-us", // Date formatting language
-  profile: "https://github.com/churchTao",
-  authorName: "Author Name", // Author name
-  email: "mailto:your-email@example.com",
+  profile: "https://github.com/ogirginc",
+  authorName: "Oğulcan Girginç", // Author name
+  // email: "mailto:email@example.com",
   tags: {
     title: "Tags", // Tags page title
     description: "All the tags used in posts.", // Tags page description
